c172fef062489ef09c53d45b7d09c3d75b4854e5
Nella1a <m.allen@gmx.at>
2022-01-13 23:21:57 +0100

    Code review

--- index.js
+++ index.js
@@ -10,31 +10,30 @@ let arrayOfTenImages = [];
 let arrayConvertObjToString = [];
 let i = 0;
 
-//request data from URL
+//request data from server
 get('https://memegen-link-examples-upleveled.netlify.app/').then((response) => {
   if (response.status === 200) {
     const html = response.data;
 
-    // extraxt URL images from server respond
+    // extraxt url-images from server respond
     arrayUrlImages = html.match(regex);
     arrayOfTenImages = Object.values(arrayUrlImages);
   } else {
     console.log(error);
   }
 
-  // looping over first 10 imgs, remove src= && "", save URL in new array
+  // looping over array, get 10 imgs, remove src= && "", save url in new array
   for (let i = 1; i < 10; i++) {
     arrayConvertObjToString[i] = arrayOfTenImages[i].substring(
       5,
       arrayOfTenImages[i].length - 1,
     );
 
-    // url string to a img
     const url = arrayConvertObjToString[i];
 
     //create a writable stream and save the received data stream to path
     https.get(url, (res) => {
-      const path = `./meme/0${i}.jpg`; //save file
+      const path = `./meme/0${i}.jpg`;
       const writeStream = fs.createWriteStream(path);
       res.pipe(writeStream);
 
