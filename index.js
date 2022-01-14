4df511678413227b0c9e664769981c39ec24789f
Nella1a <m.allen@gmx.at>
2022-01-14 13:33:03 +0100

    Remove unused code

--- index.js
+++ index.js
@@ -3,33 +3,29 @@ import https from 'node:https';
 import axios from 'axios';
 
 const regex = /src="https([^"]+)"/gi;
-// const regex = /src=['|"](https:.*?)['|"]/gi;
 const { get } = axios;
 let dataFromServer;
 let arrayUrlImages = [];
-const arrayConvertObjToString = [];
+const arrayOfTenImg = [];
 
 // request data from server
 get('https://memegen-link-examples-upleveled.netlify.app/')
   .then((response) => {
     if (response.status === 200) {
       dataFromServer = response.data;
-      console.log(typeof dataFromServer);
 
       // extraxt url-images from server respond
       arrayUrlImages = dataFromServer.match(regex);
-      console.log(typeof arrayUrlImages);
-      // arrayOfTenImages = Object.values(arrayUrlImages);
     }
 
-    // looping over array, get first 10 imgs, remove src= && "", save url in new array
+    // loop over array, get first 10 imgs, remove src= && "", save url in new array
     for (let i = 0; i < 10; i++) {
-      arrayConvertObjToString[i] = arrayUrlImages[i].slice(
+      arrayOfTenImg[i] = arrayUrlImages[i].slice(
         5,
         arrayUrlImages[i].length - 1,
       );
 
-      const url = arrayConvertObjToString[i];
+      const url = arrayOfTenImg[i];
 
       // create a writable stream and save the received data stream to path
       https.get(url, (res) => {
