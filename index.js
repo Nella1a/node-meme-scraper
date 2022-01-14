0a6b75daaf03ddb6d17ad5a08bd9e0cbc1cd88fe
Nella1a <m.allen@gmx.at>
2022-01-14 13:12:55 +0100

    Add EsLint Config

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
+import fs from 'node:fs';
+import https from 'node:https';
 import axios from 'axios';
-import fs from 'fs';
-import https from 'https';
-import request from 'request';
 
 const regex = /src="https([^"]+)"/gi;
+// const regex = /src=['|"](https:.*?)['|"]/gi;
 const { get } = axios;
-let arrayUrlImages;
-let arrayOfTenImages = [];
-let arrayConvertObjToString = [];
 let dataFromServer;
-let i = 0;
+let arrayUrlImages = [];
+const arrayConvertObjToString = [];
 
-//request data from server
-get('https://memegen-link-examples-upleveled.netlify.app/').then((response) => {
-  if (response.status === 200) {
-    dataFromServer = response.data;
+// request data from server
+get('https://memegen-link-examples-upleveled.netlify.app/')
+  .then((response) => {
+    if (response.status === 200) {
+      dataFromServer = response.data;
+      console.log(typeof dataFromServer);
 
-    // extraxt url-images from server respond
-    arrayUrlImages = dataFromServer.match(regex);
-    arrayOfTenImages = Object.values(arrayUrlImages);
-  } else {
-    console.log(error);
-  }
+      // extraxt url-images from server respond
+      arrayUrlImages = dataFromServer.match(regex);
+      console.log(typeof arrayUrlImages);
+      // arrayOfTenImages = Object.values(arrayUrlImages);
+    }
 
-  // looping over array, get first 10 imgs, remove src= && "", save url in new array
-  for (let i = 0; i < 10; i++) {
-    arrayConvertObjToString[i] = arrayOfTenImages[i].substring(
-      5,
-      arrayOfTenImages[i].length - 1,
-    );
+    // looping over array, get first 10 imgs, remove src= && "", save url in new array
+    for (let i = 0; i < 10; i++) {
+      arrayConvertObjToString[i] = arrayUrlImages[i].slice(
+        5,
+        arrayUrlImages[i].length - 1,
+      );
 
-    const url = arrayConvertObjToString[i];
+      const url = arrayConvertObjToString[i];
 
-    //create a writable stream and save the received data stream to path
-    https.get(url, (res) => {
-      let path;
+      // create a writable stream and save the received data stream to path
+      https.get(url, (res) => {
+        let path;
 
-      i !== 9
-        ? (path = `./meme/0${i + 1}.jpg`)
-        : (path = `./meme/${i + 1}.jpg`);
+        i !== 9
+          ? (path = `./meme/0${i + 1}.jpg`)
+          : (path = `./meme/${i + 1}.jpg`);
 
-      const writeStream = fs.createWriteStream(path);
-      res.pipe(writeStream);
+        const writeStream = fs.createWriteStream(path);
+        res.pipe(writeStream);
 
-      writeStream.on('finish', () => {
-        writeStream.close();
-        console.log('Download Completed');
+        writeStream.on('finish', () => {
+          writeStream.close();
+          console.log('Download Completed');
+        });
       });
-    });
-  }
-});
+    }
+  })
+  .catch((err) => console.error(err));
